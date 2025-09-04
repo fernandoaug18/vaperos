846d54af25430a57ae23eb94540aaf9cb94e3236
fernandoaug18 <67563454+fernandoaug18@users.noreply.github.com>
2025-09-04 09:27:50 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: '/',
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  plugins: [react()],
+  base: '/', // 👈 obligatorio cuando usas dominio propio (www.vaperos.cl)
+})
