48222f34b9ff8bdf0bf4298c5489292415e55e57
fernandoaug18 <67563454+fernandoaug18@users.noreply.github.com>
2025-09-04 09:39:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // 👈 obligatorio cuando usas dominio propio (www.vaperos.cl)
+  base: '/',           // 👈 obligatorio con dominio propio (www.vaperos.cl)
+  // build: { outDir: 'dist' } // (opcional) si algún día cambias la carpeta
 })
